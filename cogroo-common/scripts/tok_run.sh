5c29c783c5174c646bd31c4e4f9c3bed29391cd1
colen <colen@d547cdcf-a329-0410-8b4b-b1e8ac1ec92a>
2012-03-01 13:24:50 +0000

    added new tests

---
+++ cogroo-common/scripts/tok_run.sh
@@ -0,0 +1,17 @@
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d CF -o SD_ABB
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d CF
+
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d CF -o SD_ABB
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d CF
+
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d VCF -o SD_ABB
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d VCF
+
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d VCF -o SD_ABB
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d VCF
+
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d AMA -o SD_ABB
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d AMA
+
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d AMA -o SD_ABB
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d AMA
