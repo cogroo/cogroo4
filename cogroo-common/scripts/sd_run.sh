85d769295d41ac40c7e9269bb0ce2aeb5f560c5b
colen <colen@d547cdcf-a329-0410-8b4b-b1e8ac1ec92a>
2012-03-16 22:44:08 +0000

    Enabled SD evaluation again

--- cogroo-common/scripts/sd_run.sh
+++ cogroo-common/scripts/sd_run.sh
@@ -1,29 +1,29 @@
-# perl scripts/eval.pl -t sent -p 8 -e -v gp -a MAXENT -d CF -o SD_FACTORY,SD_ABB
-# perl scripts/eval.pl -t sent -p 8 -e -v gp -a MAXENT -d CF -o SD_FACTORY
-# perl scripts/eval.pl -t sent -p 8 -e -v gp -a MAXENT -d CF -o SD_ABB
-# perl scripts/eval.pl -t sent -p 8 -e -v gp -a MAXENT -d CF
+perl scripts/eval.pl -t sent -p 8 -e -v gp -a MAXENT -d vcf -o SD_FACTORY,SD_ABB
+perl scripts/eval.pl -t sent -p 8 -e -v gp -a MAXENT -d vcf -o SD_FACTORY
+perl scripts/eval.pl -t sent -p 8 -e -v gp -a MAXENT -d vcf -o SD_ABB
+perl scripts/eval.pl -t sent -p 8 -e -v gp -a MAXENT -d vcf
 
-# perl scripts/eval.pl -t sent -p 8 -e -v gp -a PERCEPTRON -d CF -o SD_FACTORY,SD_ABB
-# perl scripts/eval.pl -t sent -p 8 -e -v gp -a PERCEPTRON -d CF -o SD_FACTORY
-# perl scripts/eval.pl -t sent -p 8 -e -v gp -a PERCEPTRON -d CF -o SD_ABB
-# perl scripts/eval.pl -t sent -p 8 -e -v gp -a PERCEPTRON -d CF
+perl scripts/eval.pl -t sent -p 8 -e -v gp -a PERCEPTRON -d vcf -o SD_FACTORY,SD_ABB
+perl scripts/eval.pl -t sent -p 8 -e -v gp -a PERCEPTRON -d vcf -o SD_FACTORY
+perl scripts/eval.pl -t sent -p 8 -e -v gp -a PERCEPTRON -d vcf -o SD_ABB
+perl scripts/eval.pl -t sent -p 8 -e -v gp -a PERCEPTRON -d vcf
 
-# perl scripts/eval.pl -t sent -p 8 -e -v gp -a MAXENT -d VCF -o SD_FACTORY,SD_ABB
-# perl scripts/eval.pl -t sent -p 8 -e -v gp -a MAXENT -d VCF -o SD_FACTORY
-# perl scripts/eval.pl -t sent -p 8 -e -v gp -a MAXENT -d VCF -o SD_ABB
-# perl scripts/eval.pl -t sent -p 8 -e -v gp -a MAXENT -d VCF
+perl scripts/eval.pl -t sent -p 8 -e -v gp -a MAXENT -d cf -o SD_FACTORY,SD_ABB
+perl scripts/eval.pl -t sent -p 8 -e -v gp -a MAXENT -d cf -o SD_FACTORY
+perl scripts/eval.pl -t sent -p 8 -e -v gp -a MAXENT -d cf -o SD_ABB
+perl scripts/eval.pl -t sent -p 8 -e -v gp -a MAXENT -d cf
 
-# perl scripts/eval.pl -t sent -p 8 -e -v gp -a PERCEPTRON -d VCF -o SD_FACTORY,SD_ABB
-# perl scripts/eval.pl -t sent -p 8 -e -v gp -a PERCEPTRON -d VCF -o SD_FACTORY
-# perl scripts/eval.pl -t sent -p 8 -e -v gp -a PERCEPTRON -d VCF -o SD_ABB
-# perl scripts/eval.pl -t sent -p 8 -e -v gp -a PERCEPTRON -d VCF
+perl scripts/eval.pl -t sent -p 8 -e -v gp -a PERCEPTRON -d cf -o SD_FACTORY,SD_ABB
+perl scripts/eval.pl -t sent -p 8 -e -v gp -a PERCEPTRON -d cf -o SD_FACTORY
+perl scripts/eval.pl -t sent -p 8 -e -v gp -a PERCEPTRON -d cf -o SD_ABB
+perl scripts/eval.pl -t sent -p 8 -e -v gp -a PERCEPTRON -d cf
 
-# perl scripts/eval.pl -t sent -p 8 -e -v gp -a MAXENT -d AMA -o SD_FACTORY,SD_ABB
-# perl scripts/eval.pl -t sent -p 8 -e -v gp -a MAXENT -d AMA -o SD_FACTORY
-# perl scripts/eval.pl -t sent -p 8 -e -v gp -a MAXENT -d AMA -o SD_ABB
-# perl scripts/eval.pl -t sent -p 8 -e -v gp -a MAXENT -d AMA
+perl scripts/eval.pl -t sent -p 8 -e -v gp -a MAXENT -d ama -o SD_FACTORY,SD_ABB
+perl scripts/eval.pl -t sent -p 8 -e -v gp -a MAXENT -d ama -o SD_FACTORY
+perl scripts/eval.pl -t sent -p 8 -e -v gp -a MAXENT -d ama -o SD_ABB
+perl scripts/eval.pl -t sent -p 8 -e -v gp -a MAXENT -d ama
 
-perl scripts/eval.pl -t sent -p 8 -e -v gp -a PERCEPTRON -d AMA -o SD_FACTORY,SD_ABB
-perl scripts/eval.pl -t sent -p 8 -e -v gp -a PERCEPTRON -d AMA -o SD_FACTORY
-perl scripts/eval.pl -t sent -p 8 -e -v gp -a PERCEPTRON -d AMA -o SD_ABB
-perl scripts/eval.pl -t sent -p 8 -e -v gp -a PERCEPTRON -d AMA
+perl scripts/eval.pl -t sent -p 8 -e -v gp -a PERCEPTRON -d ama -o SD_FACTORY,SD_ABB
+perl scripts/eval.pl -t sent -p 8 -e -v gp -a PERCEPTRON -d ama -o SD_FACTORY
+perl scripts/eval.pl -t sent -p 8 -e -v gp -a PERCEPTRON -d ama -o SD_ABB
+perl scripts/eval.pl -t sent -p 8 -e -v gp -a PERCEPTRON -d ama
