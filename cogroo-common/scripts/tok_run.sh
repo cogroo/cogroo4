7e236b144eae2b96e62c93b8528c3c67435628c3
colen <colen@d547cdcf-a329-0410-8b4b-b1e8ac1ec92a>
2012-03-22 01:23:30 +0000

    added tokenize option  alphanumeric optimization

--- cogroo-common/scripts/tok_run.sh
+++ cogroo-common/scripts/tok_run.sh
@@ -1,14 +1,26 @@
-perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d cf -o SD_ABB
-perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d cf
+# perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d cf -o SD_ABB
+# perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d cf
 
-perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d cf -o SD_ABB
-perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d cf
+# perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d cf -o SD_ABB
+# perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d cf
 
-perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d vcf -o SD_ABB
-perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d vcf
+# perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d vcf -o SD_ABB
+# perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d vcf
 
-perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d vcf -o SD_ABB
-perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d vcf
+# perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d vcf -o SD_ABB
+# perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d vcf
+
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d cf -o SD_ABB,TOK_ALPHAOPT 
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d cf -o TOK_ALPHAOPT
+
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d cf -o SD_ABB,TOK_ALPHAOPT
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d cf -o TOK_ALPHAOPT
+
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d vcf -o SD_ABB,TOK_ALPHAOPT
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d vcf -o TOK_ALPHAOPT
+
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d vcf -o SD_ABB,TOK_ALPHAOPT
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d vcf -o TOK_ALPHAOPT
 
 
 # can't train with AMA because it never finishes
