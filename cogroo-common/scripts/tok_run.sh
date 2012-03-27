8052806872e2c5427665f85fba6d5a5be362bfcb
colen <colen@d547cdcf-a329-0410-8b4b-b1e8ac1ec92a>
2012-03-27 17:10:11 +0000

    Added more configurations to tok_run

--- cogroo-common/scripts/tok_run.sh
+++ cogroo-common/scripts/tok_run.sh
@@ -1,14 +1,31 @@
-# perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d cf -o SD_ABB
-# perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d cf
+# SEM ALPHA
 
-# perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d cf -o SD_ABB
-# perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d cf
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d cf -o SD_ABB
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d cf
 
-# perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d vcf -o SD_ABB
-# perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d vcf
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d cf -o SD_ABB
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d cf
 
-# perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d vcf -o SD_ABB
-# perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d vcf
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d vcf -o SD_ABB
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d vcf
+
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d vcf -o SD_ABB
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d vcf
+
+
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d cf -o SD_ABB,TOK_ACP
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d cf -o TOK_ACP
+
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d cf -o SD_ABB,TOK_ACP
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d cf -o TOK_ACP
+
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d vcf -o SD_ABB,TOK_ACP
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d vcf -o TOK_ACP
+
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d vcf -o SD_ABB,TOK_ACP
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d vcf -o TOK_ACP
+
+# TOK_ALPHA
 
 perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d cf -o SD_ABB,TOK_ALPHAOPT 
 perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d cf -o TOK_ALPHAOPT
@@ -21,6 +38,19 @@ perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d vcf -o TOK_ALPHAOPT
 
 perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d vcf -o SD_ABB,TOK_ALPHAOPT
 perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d vcf -o TOK_ALPHAOPT
+
+
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d cf -o SD_ABB,TOK_ALPHAOPT,TOK_ACP 
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d cf -o TOK_ALPHAOPT,TOK_ACP
+
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d cf -o SD_ABB,TOK_ALPHAOPT,TOK_ACP
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d cf -o TOK_ALPHAOPT,TOK_ACP
+
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d vcf -o SD_ABB,TOK_ALPHAOPT,TOK_ACP
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a MAXENT -d vcf -o TOK_ALPHAOPT,TOK_ACP
+
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d vcf -o SD_ABB,TOK_ALPHAOPT,TOK_ACP
+perl scripts/eval.pl -t tok -p 8 -e -v gp -a PERCEPTRON -d vcf -o TOK_ALPHAOPT,TOK_ACP
 
 
 # can't train with AMA because it never finishes
