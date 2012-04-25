53c3b3cc27754b75bf9b01cf27a4b52b8dd3fca1
arthbr <arthbr@d547cdcf-a329-0410-8b4b-b1e8ac1ec92a>
2012-04-25 20:45:17 +0000

    Creation of new pipe. #Fixes 598

---
+++ cogroo-base/src/main/java/br/ccsl/cogroo/Pipe.java
@@ -0,0 +1,74 @@
+package br.ccsl.cogroo;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import org.apache.log4j.Logger;
+
+import com.google.common.io.Closeables;
+
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+import opennlp.tools.util.Span;
+
+public class Pipe {
+
+  protected static final Logger LOGGER = Logger.getLogger(Pipe.class);
+  private SentenceDetectorME sentenceDetector;
+  
+  public Pipe() throws FileNotFoundException {
+    InputStream modelIn = new FileInputStream("models/pt-sent.bin");
+
+    try {
+      SentenceModel model = new SentenceModel(modelIn);
+      sentenceDetector = new SentenceDetectorME(model);
+    } catch (IOException e) {
+      LOGGER.fatal("Couldn't load sentence model!", e);
+    } finally {
+      Closeables.closeQuietly(modelIn);
+    }
+
+  }
+  
+  public Span[] analyze (String text) {
+    Span sentences[] = sentenceDetector.sentPosDetect(text);
+    if ( LOGGER.isDebugEnabled() ) {
+      LOGGER.debug("Found sentences: " + Arrays.toString( Span.spansToStrings(sentences, text)) );
+    }
+    
+    return sentences;
+  }
+
+  /**
+   * @param args
+   * @throws FileNotFoundException 
+   */
+  public static void main(String[] args) throws FileNotFoundException {
+   
+    long start = System.nanoTime();
+    Pipe pipe = new Pipe();
+    
+    System.out.println("Loading time ["
+            + ((System.nanoTime() - start) / 1000000) + "ms]");
+    Scanner kb = new Scanner(System.in);
+    System.out.print("Enter the sentence: ");
+    String input = kb.nextLine();
+    while (!input.equals("q")) {
+        if (input.equals("0")) {
+            input = "Fomos levados à crer que os menino são burro de doer. As menina chegaram.";
+        }
+        
+        pipe.analyze(input);
+
+        System.out.print("Enter the sentence: ");
+        input = kb.nextLine();
+    }
+    
+    
+  }
+
+}
