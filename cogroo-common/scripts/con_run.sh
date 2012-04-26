9796368d373645de35806dd24403708ecd20513f
colen <colen@d547cdcf-a329-0410-8b4b-b1e8ac1ec92a>
2012-04-26 12:34:21 +0000

    Will try to continue evaluating with VCF and AMA. refs #600

--- cogroo-common/scripts/con_run.sh
+++ cogroo-common/scripts/con_run.sh
@@ -1,8 +1,8 @@
-perl scripts/eval.pl -t con -p 8 -e -v gp -a MAXENT -d cf -o CON_DIC
-perl scripts/eval.pl -t con -p 8 -e -v gp -a MAXENT -d cf
+# perl scripts/eval.pl -t con -p 8 -e -v gp -a MAXENT -d cf -o CON_DIC
+# perl scripts/eval.pl -t con -p 8 -e -v gp -a MAXENT -d cf
 
-perl scripts/eval.pl -t con -p 8 -e -v gp -a PERCEPTRON -d cf -o CON_DIC
-perl scripts/eval.pl -t con -p 8 -e -v gp -a PERCEPTRON -d cf
+# perl scripts/eval.pl -t con -p 8 -e -v gp -a PERCEPTRON -d cf -o CON_DIC
+# perl scripts/eval.pl -t con -p 8 -e -v gp -a PERCEPTRON -d cf
 
 perl scripts/eval.pl -t con -p 8 -e -v gp -a MAXENT -d vcf -o CON_DIC
 perl scripts/eval.pl -t con -p 8 -e -v gp -a MAXENT -d vcf
