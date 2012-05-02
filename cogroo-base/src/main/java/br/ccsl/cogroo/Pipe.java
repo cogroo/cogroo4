2cce8afdcab5c320e47ec723e45769201bf7f8fa
arthbr <arthbr@d547cdcf-a329-0410-8b4b-b1e8ac1ec92a>
2012-05-02 17:52:37 +0000

    Tokenizer and POS-tagger included in the pipe. Fixes #598.

--- cogroo-base/src/main/java/br/ccsl/cogroo/Pipe.java
+++ cogroo-base/src/main/java/br/ccsl/cogroo/Pipe.java
@@ -11,14 +11,21 @@ import org.apache.log4j.Logger;
 
 import com.google.common.io.Closeables;
 
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSTaggerME;
 import opennlp.tools.sentdetect.SentenceDetectorME;
 import opennlp.tools.sentdetect.SentenceModel;
+import opennlp.tools.tokenize.Tokenizer;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
 import opennlp.tools.util.Span;
 
 public class Pipe {
 
   protected static final Logger LOGGER = Logger.getLogger(Pipe.class);
   private SentenceDetectorME sentenceDetector;
+  private TokenizerME tokenizer;
+  private POSTaggerME tagger;
 
   public Pipe() throws FileNotFoundException {
     InputStream modelIn = new FileInputStream("models/pt-sent.bin");
@@ -31,18 +38,85 @@ public class Pipe {
     } finally {
       Closeables.closeQuietly(modelIn);
     }
+    
+    modelIn = new FileInputStream("models/pt-token.bin");
 
+    try {
+      TokenizerModel model = new TokenizerModel(modelIn);
+      tokenizer = new TokenizerME(model);
+    }
+    catch (IOException e) {
+      LOGGER.fatal("Couldn't load tokenizer model!", e);
+    }
+    finally {
+      Closeables.closeQuietly(modelIn);
+    }
+    
+    modelIn = new FileInputStream("models/pt-pos-maxent.bin");
+
+    try {
+      POSModel model = new POSModel(modelIn);
+      tagger = new POSTaggerME(model);
+    }
+    catch (IOException e) {
+      LOGGER.fatal("Couldn't load tokenizer model!", e);
+    }
+    finally {
+      Closeables.closeQuietly(modelIn);
+    }
+    
+    
   }
 
-  public Span[] analyze(String text) {
+  public void analyze(String text) {
+    Span sentenceSpan[] = analyzeSentences (text);
+    
+    for (Span span : sentenceSpan) {
+      String sentence = span.getCoveredText(text).toString();
+      
+      Span[] tokensSpan = analyzeTokens(sentence);
+      
+      String[] tokens = Span.spansToStrings(tokensSpan, sentence);
+      
+      tokens = analyzeTags(tokens);
+    }
+    
+    
+    
+    
+    
+  }
+  
+  public Span[] analyzeSentences(String text) {
     Span sentences[] = sentenceDetector.sentPosDetect(text);
     if (LOGGER.isDebugEnabled()) {
       LOGGER.debug("Found sentences: "
           + Arrays.toString(Span.spansToStrings(sentences, text)));
     }
-
+    
     return sentences;
   }
+  
+  public Span[] analyzeTokens(String text) {
+    Span tokens[] = tokenizer.tokenizePos(text);
+    if (LOGGER.isDebugEnabled()) {
+      LOGGER.debug("Tokens: " + Arrays.toString(Span.spansToStrings(tokens, text)));
+    }
+    
+    return tokens;
+  }
+  
+  public String[] analyzeTags(String[] tokens) {
+    String[] tag = tagger.tag(tokens);
+    
+    if (LOGGER.isDebugEnabled()) {
+      LOGGER.debug("Tags: " + Arrays.toString(tag));
+    }
+    
+    return tag;
+  }
+  
+  
 
   /**
    * @param args
