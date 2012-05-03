335c4634157234b441c351cb978eb547f554ce77
arthbr <arthbr@d547cdcf-a329-0410-8b4b-b1e8ac1ec92a>
2012-05-03 15:55:36 +0000

    Name Finder and Contraction Finder modules were added to the pipe. Fixes #589.

--- cogroo-base/src/main/java/br/ccsl/cogroo/Pipe.java
+++ cogroo-base/src/main/java/br/ccsl/cogroo/Pipe.java
@@ -4,13 +4,20 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 import org.apache.log4j.Logger;
 
+import br.ccsl.cogroo.entities.Sentence;
+
 import com.google.common.io.Closeables;
 
+import opennlp.tools.namefind.NameFinderME;
+import opennlp.tools.namefind.TokenNameFinder;
+import opennlp.tools.namefind.TokenNameFinderModel;
 import opennlp.tools.postag.POSModel;
 import opennlp.tools.postag.POSTaggerME;
 import opennlp.tools.sentdetect.SentenceDetectorME;
@@ -26,9 +33,11 @@ public class Pipe {
   private SentenceDetectorME sentenceDetector;
   private TokenizerME tokenizer;
   private POSTaggerME tagger;
+  private NameFinderME nameFinder;
+  private NameFinderME contractionFinder;
 
   public Pipe() throws FileNotFoundException {
-    InputStream modelIn = new FileInputStream("models/pt-sent.bin");
+    InputStream modelIn = new FileInputStream("models/pt-sent.model");
 
     try {
       SentenceModel model = new SentenceModel(modelIn);
@@ -38,86 +47,159 @@ public class Pipe {
     } finally {
       Closeables.closeQuietly(modelIn);
     }
-    
-    modelIn = new FileInputStream("models/pt-token.bin");
+
+    modelIn = new FileInputStream("models/pt-tok.model");
 
     try {
       TokenizerModel model = new TokenizerModel(modelIn);
       tokenizer = new TokenizerME(model);
-    }
-    catch (IOException e) {
+    } catch (IOException e) {
       LOGGER.fatal("Couldn't load tokenizer model!", e);
-    }
-    finally {
+    } finally {
       Closeables.closeQuietly(modelIn);
     }
-    
+
     modelIn = new FileInputStream("models/pt-pos-maxent.bin");
 
     try {
       POSModel model = new POSModel(modelIn);
       tagger = new POSTaggerME(model);
+    } catch (IOException e) {
+      LOGGER.fatal("Couldn't load POS-tagger model!", e);
+    } finally {
+      Closeables.closeQuietly(modelIn);
     }
-    catch (IOException e) {
-      LOGGER.fatal("Couldn't load tokenizer model!", e);
-    }
-    finally {
+
+    modelIn = new FileInputStream("models/pt-prop.model");
+
+    try {
+      TokenNameFinderModel model = new TokenNameFinderModel(modelIn);
+      nameFinder = new NameFinderME(model);
+    } catch (IOException e) {
+      LOGGER.fatal("Couldn't load name finder model!", e);
+    } finally {
       Closeables.closeQuietly(modelIn);
     }
     
+    modelIn = new FileInputStream("models/pt-con.model");
+
+    try {
+      TokenNameFinderModel model = new TokenNameFinderModel(modelIn);
+      contractionFinder = new NameFinderME(model);
+    } catch (IOException e) {
+      LOGGER.fatal("Couldn't load contractions finder model!", e);
+    } finally {
+      Closeables.closeQuietly(modelIn);
+    }
     
   }
 
   public void analyze(String text) {
-    Span sentenceSpan[] = analyzeSentences (text);
+    Span sentenceSpan[] = analyzeSentences(text);
     
     for (Span span : sentenceSpan) {
-      String sentence = span.getCoveredText(text).toString();
-      
-      Span[] tokensSpan = analyzeTokens(sentence);
-      
-      String[] tokens = Span.spansToStrings(tokensSpan, sentence);
-      
-      tokens = analyzeTags(tokens);
+      String sentenceString = span.getCoveredText(text).toString();
+
+      Span[] tokensSpan = analyzeTokens(sentenceString);
+
+      String[] tokens = Span.spansToStrings(tokensSpan, sentenceString);
+      tokens = findNames(tokens);
+      tokens = findContractions(tokens);
+
+      String[] tags = analyzeTags(tokens);
     }
-    
-    
-    
-    
-    
   }
-  
+
   public Span[] analyzeSentences(String text) {
     Span sentences[] = sentenceDetector.sentPosDetect(text);
     if (LOGGER.isDebugEnabled()) {
       LOGGER.debug("Found sentences: "
           + Arrays.toString(Span.spansToStrings(sentences, text)));
     }
-    
+
     return sentences;
   }
-  
+
   public Span[] analyzeTokens(String text) {
     Span tokens[] = tokenizer.tokenizePos(text);
     if (LOGGER.isDebugEnabled()) {
-      LOGGER.debug("Tokens: " + Arrays.toString(Span.spansToStrings(tokens, text)));
+      LOGGER.debug("Tokens: "
+          + Arrays.toString(Span.spansToStrings(tokens, text)));
+    }
+
+    return tokens;
+  }
+
+  public String[] analyzeTags(String[] tokens) {
+    String[] tag = tagger.tag(tokens);
+
+    if (LOGGER.isDebugEnabled()) {
+      LOGGER.debug("Tags: " + Arrays.toString(tag));
+    }
+
+    return tag;
+  }
+
+  public String[] findNames(String[] tokens) {
+    Span[] namesSpan = nameFinder.find(tokens);
+
+    if (namesSpan.length > 0) {
+      List<String> newTokens = new ArrayList<String>();
+      // i -> namesSpan; j -> tokens  
+      for (int i = 0, j = 0; j < tokens.length; j++)
+        
+          if (i < namesSpan.length && namesSpan[i].contains(j)) {
+            String name = tokens[j++];
+            for (; j < namesSpan[i].getEnd(); j++) {
+              name = name + "_" + tokens[j];
+            }
+            j--;
+            newTokens.add(name);
+            i++;
+          }
+          else
+            newTokens.add(tokens[j]);
+      
+      tokens = newTokens.toArray(new String[newTokens.size()]);
+      LOGGER.debug("Tokens com nomes: " + Arrays.toString(tokens));
+      }
+    
+    if (LOGGER.isDebugEnabled()) {
+      LOGGER.debug("Names: " + Arrays.toString(namesSpan));
+    }
+    
+    return tokens;
+  }
+
+  public String[] findContractions(String[] tokens) {
+    Span[] contractionsSpan = contractionFinder.find(tokens);
+    
+    if (contractionsSpan.length > 0) {
+      List<String> newTokens = new ArrayList<String>();
+      // i -> contractionsSpan; j -> tokens  
+      for (int i = 0, j = 0; j < tokens.length; j++)
+        
+          if (i < contractionsSpan.length && contractionsSpan[i].contains(j)) {
+            String[] contraction = ContractionUtility.expand(tokens[j]);
+            
+            for (int k = 0; k < contraction.length; k++)
+              newTokens.add(contraction[k]);
+            i++;
+          }
+          else
+            newTokens.add(tokens[j]);
+      
+      tokens = newTokens.toArray(new String[newTokens.size()]);
+      LOGGER.debug("Tokens com contrações: " + Arrays.toString(tokens));
+      }
+    
+    if (LOGGER.isDebugEnabled()) {
+      LOGGER.debug("Contrações: " + Arrays.toString(contractionsSpan));
     }
     
     return tokens;
   }
   
-  public String[] analyzeTags(String[] tokens) {
-    String[] tag = tagger.tag(tokens);
-    
-    if (LOGGER.isDebugEnabled()) {
-      LOGGER.debug("Tags: " + Arrays.toString(tag));
-    }
-    
-    return tag;
-  }
-  
-  
-
   /**
    * @param args
    * @throws FileNotFoundException
