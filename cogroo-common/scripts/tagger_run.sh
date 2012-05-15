e4cb936e217befd9e8064e98056b294a4f7ce8f3
colen <colen@d547cdcf-a329-0410-8b4b-b1e8ac1ec92a>
2012-05-15 07:20:03 +0000

    Added more eval tests refs #604

--- cogroo-common/scripts/tagger_run.sh
+++ cogroo-common/scripts/tagger_run.sh
@@ -1,16 +1,18 @@
 # expand_me & factory em todos
-perl scripts/eval.pl -t pos -p 8 -e -v gp -a MAXENT -d cf -o POS_EXPANDME,POS_FACTORY
+perl scripts/eval.pl -t pos -p 8 -e -v gp -a MAXENT -d cf
 
-perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf -o POS_EXPANDME,POS_FACTORY
+perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf -o POS_AC
 
-perl scripts/eval.pl -t pos -p 8 -e -v gp -a MAXENT -d cf -o POS_EXPANDME,POS_FACTORY,POS_DIC
+perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf -o POS_DICCUT1
 
-perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf -o POS_EXPANDME,POS_FACTORY,POS_DIC
+perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf -o POS_DIC
 
-perl scripts/eval.pl -t pos -p 8 -e -v gp -a MAXENT -d vcf -o POS_EXPANDME,POS_FACTORY
+perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf -o POS_AC,POS_DICCUT1
 
-perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d vcf -o POS_EXPANDME,POS_FACTORY
+perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf -o POS_AC,POS_DIC
 
-perl scripts/eval.pl -t pos -p 8 -e -v gp -a MAXENT -d vcf -o POS_EXPANDME,POS_FACTORY,POS_DIC
+perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf -o POS_DIC,POS_DICCUT1
 
-perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d vcf -o POS_EXPANDME,POS_FACTORY,POS_DIC
+perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf -o POS_AC,POS_DIC,POS_DICCUT1
+
+
