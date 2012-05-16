4d5ae6ab0ba8f41b3909e670fae3b95f20b52424
colen <colen@d547cdcf-a329-0410-8b4b-b1e8ac1ec92a>
2012-05-16 01:05:43 +0000

    try maxent configuration... refs #604

--- cogroo-common/scripts/tagger_run.sh
+++ cogroo-common/scripts/tagger_run.sh
@@ -1,6 +1,6 @@
 # expand_me & factory em todos
 
-perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf
+# perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf
 
 # perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf -o POS_AC
 
@@ -20,9 +20,9 @@ perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf
 
 # perl scripts/eval.pl -t pos -p 8 -e -v gp -a MAXENT -d cf
 
-perl scripts/eval.pl -t pos -p 8 -e -v gp -a MAXENT -d cf -o POS_AC
+# perl scripts/eval.pl -t pos -p 8 -e -v gp -a MAXENT -d cf -o POS_AC
 
-perl scripts/eval.pl -t pos -p 8 -e -v gp -a MAXENT -d cf -o POS_DICCUT1
+# perl scripts/eval.pl -t pos -p 8 -e -v gp -a MAXENT -d cf -o POS_DICCUT1
 
 perl scripts/eval.pl -t pos -p 8 -e -v gp -a MAXENT -d cf -o POS_DIC
 
