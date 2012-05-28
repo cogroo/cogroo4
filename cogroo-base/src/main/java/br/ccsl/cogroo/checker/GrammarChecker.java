0a420b9c8e4db1b67f3b79b137957378b515f195
colen <colen@d547cdcf-a329-0410-8b4b-b1e8ac1ec92a>
2012-05-28 13:08:47 +0000

    Code format. refs #610

--- cogroo-base/src/main/java/br/ccsl/cogroo/checker/GrammarChecker.java
+++ cogroo-base/src/main/java/br/ccsl/cogroo/checker/GrammarChecker.java
@@ -1,11 +1,9 @@
 package br.ccsl.cogroo.checker;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Locale;
 import java.util.Scanner;
 
-import br.ccsl.cogroo.analyzer.AnalyzerI;
 import br.ccsl.cogroo.analyzer.ComponentFactory;
 import br.ccsl.cogroo.analyzer.Pipe;
 import br.ccsl.cogroo.text.Document;
@@ -15,11 +13,13 @@ import br.ccsl.cogroo.text.Document;
  */
 public class GrammarChecker {
   /**
-   * @param args the language to be used, "pt_BR" by default
-   * @throws IOException 
-   * @throws IllegalArgumentException 
+   * @param args
+   *          the language to be used, "pt_BR" by default
+   * @throws IOException
+   * @throws IllegalArgumentException
    */
-  public static void main(String[] args) throws IllegalArgumentException, IOException {
+  public static void main(String[] args) throws IllegalArgumentException,
+      IOException {
 
     long start = System.nanoTime();
 
@@ -28,8 +28,7 @@ public class GrammarChecker {
       return;
     }
 
-    ComponentFactory factory = ComponentFactory
-        .create(new Locale(args[0]));
+    ComponentFactory factory = ComponentFactory.create(new Locale(args[0]));
 
     Pipe pipe = (Pipe) factory.createPipe();
     pipe.add(new GrammarCheckerAnalyzer());
