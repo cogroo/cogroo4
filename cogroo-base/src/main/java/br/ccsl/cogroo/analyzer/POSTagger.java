068adb46fa091eb07ab1ba21c5a117cea6011fb5
colen <colen@d547cdcf-a329-0410-8b4b-b1e8ac1ec92a>
2012-05-28 12:59:52 +0000

    Change POSTagger analyzer to group MWE. refs #598

--- cogroo-base/src/main/java/br/ccsl/cogroo/analyzer/POSTagger.java
+++ cogroo-base/src/main/java/br/ccsl/cogroo/analyzer/POSTagger.java
@@ -1,15 +1,17 @@
 package br.ccsl.cogroo.analyzer;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import opennlp.tools.postag.POSTaggerME;
-
+import opennlp.tools.util.Span;
 import br.ccsl.cogroo.config.Analyzers;
 import br.ccsl.cogroo.text.Document;
 import br.ccsl.cogroo.text.Sentence;
 import br.ccsl.cogroo.text.Token;
 import br.ccsl.cogroo.text.impl.TokenImpl;
+import br.ccsl.cogroo.util.EntityUtils;
 import br.ccsl.cogroo.util.TextUtils;
 
 /**
@@ -37,6 +39,61 @@ public class POSTagger implements AnalyzerI {
       for (int i = 0; i < tags.length; i++) {
         ((TokenImpl) tokens.get(i)).setPOSTag(tags[i]);
       }
+      
+      EntityUtils.groupTokens(sentence.getText(), tokens, createSpanList(toTokensArray(tokens), toTagsArray(tokens)));
     }
+  }
+  
+  private String[] toTokensArray(List<Token> tokens) {
+    String[] arr = new String[tokens.size()];
+    for (int i = 0; i < tokens.size(); i++) {
+      arr[i] = tokens.get(i).getLexeme();
+    }
+    return arr;
+  }
+
+  private String[] toTagsArray(List<Token> tokens) {
+    String[] arr = new String[tokens.size()];
+    for (int i = 0; i < tokens.size(); i++) {
+      arr[i] = tokens.get(i).getPOSTag();
+    }
+    return arr;
+  }
+
+  // this is from opennlp
+  public static List<Span> createSpanList(String[] toks, String[] tags) {
+
+    // initialize with the list maximum size
+    List<Span> phrases = new ArrayList<Span>(toks.length); 
+    String startTag = "";
+    int startIndex = 0;
+    boolean foundPhrase = false;
+
+    for (int ci = 0, cn = tags.length; ci < cn; ci++) {
+      String pred = tags[ci];
+      if(!tags[ci].startsWith("B-") && !tags[ci].startsWith("I-")) {
+        pred = "O";
+      }
+      if (pred.startsWith("B-")
+          || (!pred.equals("I-" + startTag) && !pred.equals("O"))) { // start
+        if (foundPhrase) { // handle the last
+          phrases.add(new Span(startIndex, ci, startTag));
+        }
+        startIndex = ci;
+        startTag = pred.substring(2);
+        foundPhrase = true;
+      } else if (pred.equals("I-" + startTag)) { // middle
+        // do nothing
+      } else if (foundPhrase) {// end
+        phrases.add(new Span(startIndex, ci, startTag));
+        foundPhrase = false;
+        startTag = "";
+      }
+    }
+    if (foundPhrase) { // leftover
+      phrases.add(new Span(startIndex, tags.length, startTag));
+    }
+
+    return phrases;
   }
 }
