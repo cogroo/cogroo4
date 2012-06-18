0b2da4cfff8211787b07cd496547c5008928aadf
colen <colen@d547cdcf-a329-0410-8b4b-b1e8ac1ec92a>
2012-06-18 14:32:46 +0000

    Improved featurizer #606

---
+++ cogroo-common/src/test/java/br/ccsl/cogroo/tools/featurizer/DefaultFeaturizerSequenceValidatorTest.java
@@ -0,0 +1,27 @@
+package br.ccsl.cogroo.tools.featurizer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DefaultFeaturizerSequenceValidatorTest {
+
+  @Test
+  public void testMatches() {
+    assertFalse(DefaultFeaturizerSequenceValidator.matches("V=PCP=F=S", "F=S"));
+    
+    assertTrue(DefaultFeaturizerSequenceValidator.matches("0/1S", "0/1/3S"));
+    assertTrue(DefaultFeaturizerSequenceValidator.matches("1S", "0/1/3S"));
+    assertFalse(DefaultFeaturizerSequenceValidator.matches("2S", "0/1/3S"));
+    assertFalse(DefaultFeaturizerSequenceValidator.matches("1P", "0/1/3S"));
+    assertFalse(DefaultFeaturizerSequenceValidator.matches("3P", "0/1/3S"));
+    
+    assertTrue(DefaultFeaturizerSequenceValidator.matches("M=S", "M/F=S/P"));
+    assertTrue(DefaultFeaturizerSequenceValidator.matches("M=S", "M/F=S"));
+    
+    assertFalse(DefaultFeaturizerSequenceValidator.matches("M", "M/F=S"));
+    
+    
+  }
+
+}
