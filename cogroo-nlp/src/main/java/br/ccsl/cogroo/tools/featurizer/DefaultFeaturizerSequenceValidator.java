325b8219c9f9d68511019e7d29f46d83979fa30a
William Colen <william.colen@gmail.com>
2012-06-20 00:44:12 -0300

    Removed unused code refs #613

--- cogroo-nlp/src/main/java/br/ccsl/cogroo/tools/featurizer/DefaultFeaturizerSequenceValidator.java
+++ cogroo-nlp/src/main/java/br/ccsl/cogroo/tools/featurizer/DefaultFeaturizerSequenceValidator.java
@@ -23,11 +23,9 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.regex.Pattern;
 
 import opennlp.tools.util.SequenceValidator;
 import br.ccsl.cogroo.dictionary.FeatureDictionaryI;
-import br.ccsl.cogroo.interpreters.FlorestaTagInterpreter;
 
 public class DefaultFeaturizerSequenceValidator implements
     SequenceValidator<WordTag> {
@@ -130,8 +128,6 @@ public class DefaultFeaturizerSequenceValidator implements
     }
     return Collections.unmodifiableList(filtered);
   }
-
-  FlorestaTagInterpreter ti = new FlorestaTagInterpreter();
 
   private boolean matches(String outcome, List<String> tags) {
     if(tags.contains(outcome)) {
