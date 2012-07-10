d0d332d6fe9ffa73799535679ca4e5627f30a4f8
William Colen <william.colen@gmail.com>
2012-07-10 11:45:13 -0300

    Updated license of -gc

--- cogroo-gc/src/main/java/org/cogroo/tools/checker/checkers/PunctuationChecker.java
+++ cogroo-gc/src/main/java/org/cogroo/tools/checker/checkers/PunctuationChecker.java
@@ -1,30 +1,18 @@
 /**
- * Copyright (C) 2008 CoGrOO Team (cogroo AT gmail DOT com)
- * 
- * CoGrOO Team (cogroo AT gmail DOT com)
- * LTA, PCS (Computer and Digital Systems Engineering Department),
- * Escola Politécnica da Universidade de São Paulo
- * Av. Prof. Luciano Gualberto, trav. 3, n. 380
- * CEP 05508-900 - São Paulo - SP - BRAZIL
- * 
- * http://cogroo.sourceforge.net/
- * 
- * This file is part of CoGrOO.
- * 
- * CoGrOO is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public as published by 
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * CoGrOO is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with CoGrOO. If not, see <http://www.gnu.org/licenses/>.
+ * Copyright (C) 2012 cogroo <cogroo@cogroo.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package org.cogroo.tools.checker.checkers;
 
 import java.util.LinkedList;
