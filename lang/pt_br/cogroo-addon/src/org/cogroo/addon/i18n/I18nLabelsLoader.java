683218e5729a3f1215ce467723bb02848463b2b6
William Colen <william.colen@gmail.com>
2012-07-10 13:13:16 -0300

    Updated license of lang subprojects

--- lang/pt_br/cogroo-addon/src/org/cogroo/addon/i18n/I18nLabelsLoader.java
+++ lang/pt_br/cogroo-addon/src/org/cogroo/addon/i18n/I18nLabelsLoader.java
@@ -1,43 +1,18 @@
 /**
- * Copyright (C) 2008 William Colen<colen at users.sourceforge.net>
- * 
- * http://lingualquanta.sourceforge.net/ooointegration
- * 
- * This file is part of Lingual Quanta OpenOffice.org Integration.
- * 
- * OOoIntegration is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Publicas published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * OOoIntegration is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with OOoIntegration.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * NOTICE:
- * Some peaces of the code came from other projects I studied. 
- * I have to thanks the developers of:
- * 	CoGrOO
- * 		Site: cogroo.sourceforge.net 
- * 		License: LGPL
- * 		Used as a sample of Grammar Checker Addon.
- * 	LinguageTool 
- * 		Site: www.languagetool.org
- * 		License: LGPL
- * 		Used as a sample of Grammar Checker Addon that
- * 		implements the XGrammarChecker interface.
- *  	dxf2calc 
- *  		Site: www.abj.dk/dxf2calc
- * 		License: SODA-WARE
- *  		Was used as more sofisticated sample of OOo Addon, specially 
- *  		as a sample of how to find the Addon folder during runtime and
- *  		how do a nice About dialog box. 
+ * Copyright (C) 2012 cogroo <cogroo@cogroo.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package org.cogroo.addon.i18n;
 
 import java.util.ResourceBundle;
