8b61ad4d4bc62ac413ef97bb99e739723c8b3177
William Colen <william.colen@gmail.com>
2012-07-12 13:29:31 -0300

    Separated specific Tagger features to make evaluation easier

---
+++ cogroo-nlp/src/main/java/org/cogroo/tools/postag/PortugueseExFactory.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (C) 2012 cogroo <cogroo@cogroo.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cogroo.tools.postag;
+
+import opennlp.tools.dictionary.Dictionary;
+import opennlp.tools.postag.POSContextGenerator;
+import opennlp.tools.postag.TagDictionary;
+import opennlp.tools.util.model.ArtifactProvider;
+
+public class PortugueseExFactory extends PortugueseFactory {
+
+  public PortugueseExFactory(Dictionary ngramDictionary,
+      TagDictionary posDictionary) {
+    super(ngramDictionary, posDictionary);
+  }
+
+  public PortugueseExFactory(ArtifactProvider artifactProvider) {
+    super(artifactProvider);
+  }
+
+  @Override
+  public POSContextGenerator getPOSContextGenerator(int cacheSize) {
+    return new PortugueseExtPOSContextGenerator(cacheSize, getDictionary());
+  }
+
+  @Override
+  public POSContextGenerator getPOSContextGenerator() {
+    return new PortugueseExtPOSContextGenerator(getDictionary());
+  }
+  
+}
