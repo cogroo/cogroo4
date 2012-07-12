396971916710052895d063c05e78701e60915cf5
William Colen <william.colen@gmail.com>
2012-07-12 13:39:57 -0300

    Updated to execute Ex Factory

--- cogroo-nlp/scripts/tagger_run.sh
+++ cogroo-nlp/scripts/tagger_run.sh
@@ -16,38 +16,41 @@
 
 # expand_me & factory em todos
 
-perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf
+perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf -o POS_FACT
 
-perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf -o POS_AC
+perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf -o POS_FACT,POS_AC
 
-perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf -o POS_DICCUT1
+perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf -o POS_FACT,POS_DICCUT1
 
-perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf -o POS_DIC
+perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf -o POS_FACT,POS_DIC
 
-perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf -o POS_AC,POS_DICCUT1
+perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf -o POS_FACT,POS_AC,POS_DICCUT1
 
-perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf -o POS_AC,POS_DIC
+perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf -o POS_FACT,POS_AC,POS_DIC
 
-perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf -o POS_DIC,POS_DICCUT1
+perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf -o POS_FACT,POS_DIC,POS_DICCUT1
 
-perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf -o POS_AC,POS_DIC,POS_DICCUT1
+perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf -o POS_FACT,POS_AC,POS_DIC,POS_DICCUT1
+
+perl scripts/eval.pl -t pos -p 8 -e -v gp -a PERCEPTRON -d cf -o POS_EXFACT,POS_AC,POS_DIC,POS_DICCUT1
 
 ##
 
-perl scripts/eval.pl -t pos -p 8 -e -v gp -a MAXENT -d cf
+perl scripts/eval.pl -t pos -p 8 -e -v gp -a MAXENT -d cf -o POS_FACT
 
-perl scripts/eval.pl -t pos -p 8 -e -v gp -a MAXENT -d cf -o POS_AC
+perl scripts/eval.pl -t pos -p 8 -e -v gp -a MAXENT -d cf -o POS_FACT,POS_AC
 
-perl scripts/eval.pl -t pos -p 8 -e -v gp -a MAXENT -d cf -o POS_DICCUT1
+perl scripts/eval.pl -t pos -p 8 -e -v gp -a MAXENT -d cf -o POS_FACT,POS_DICCUT1
 
-perl scripts/eval.pl -t pos -p 8 -e -v gp -a MAXENT -d cf -o POS_DIC
+perl scripts/eval.pl -t pos -p 8 -e -v gp -a MAXENT -d cf -o POS_FACT,POS_DIC
 
-perl scripts/eval.pl -t pos -p 8 -e -v gp -a MAXENT -d cf -o POS_AC,POS_DICCUT1
+perl scripts/eval.pl -t pos -p 8 -e -v gp -a MAXENT -d cf -o POS_FACT,POS_AC,POS_DICCUT1
 
-perl scripts/eval.pl -t pos -p 8 -e -v gp -a MAXENT -d cf -o POS_AC,POS_DIC
+perl scripts/eval.pl -t pos -p 8 -e -v gp -a MAXENT -d cf -o POS_FACT,POS_AC,POS_DIC
 
-perl scripts/eval.pl -t pos -p 8 -e -v gp -a MAXENT -d cf -o POS_DIC,POS_DICCUT1
+perl scripts/eval.pl -t pos -p 8 -e -v gp -a MAXENT -d cf -o POS_FACT,POS_DIC,POS_DICCUT1
 
-perl scripts/eval.pl -t pos -p 8 -e -v gp -a MAXENT -d cf -o POS_AC,POS_DIC,POS_DICCUT1
+perl scripts/eval.pl -t pos -p 8 -e -v gp -a MAXENT -d cf -o POS_FACT,POS_AC,POS_DIC,POS_DICCUT1
 
+perl scripts/eval.pl -t pos -p 8 -e -v gp -a MAXENT -d cf -o POS_EXFACT,POS_AC,POS_DIC,POS_DICCUT1
 
