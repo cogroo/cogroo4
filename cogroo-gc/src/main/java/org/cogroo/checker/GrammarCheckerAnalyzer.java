bec8ab2757ded9237c41262b81c6a1f28cccd1ab
William Colen <william.colen@gmail.com>
2012-08-09 12:09:08 -0300

    Disable WordCombinationChecker and enabled chunk and syntactic rules.

--- cogroo-gc/src/main/java/org/cogroo/checker/GrammarCheckerAnalyzer.java
+++ cogroo-gc/src/main/java/org/cogroo/checker/GrammarCheckerAnalyzer.java
@@ -29,7 +29,6 @@ import org.cogroo.text.Document;
 import org.cogroo.text.Sentence;
 import org.cogroo.tools.checker.Checker;
 import org.cogroo.tools.checker.CheckerComposite;
-import org.cogroo.tools.checker.GenericChecker;
 import org.cogroo.tools.checker.TypedChecker;
 import org.cogroo.tools.checker.TypedCheckerAdapter;
 import org.cogroo.tools.checker.TypedCheckerComposite;
@@ -112,7 +111,7 @@ public class GrammarCheckerAnalyzer implements AnalyzerI {
     
     // finally:
     checkerList.add(adaptedComposite);
-    checkerList.add(new WordCombinationChecker());
+//    checkerList.add(new WordCombinationChecker());
     
 
     // now we can create other checkers...
