4d6ed076fcab6487d3c73f94aab1139ff51f55f4
William Colen <william.colen@gmail.com>
2012-08-09 12:08:36 -0300

    Fixed a bug reported by Michel
    The nameFinder was not setting the span of the token properly. Bug was
    causing exception.

--- cogroo-ann/src/main/java/org/cogroo/analyzer/NameFinder.java
+++ cogroo-ann/src/main/java/org/cogroo/analyzer/NameFinder.java
@@ -52,6 +52,9 @@ public class NameFinder implements AnalyzerI {
       for (int i = namesSpan.length - 1; i >= 0; i--) {
         int start = namesSpan[i].getStart(), end = namesSpan[i].getEnd();
 
+        int chStart = newTokens.get(start).getStart();
+        int chEnd = newTokens.get(end - 1).getEnd();
+
         String name = newTokens.get(end - 1).getLexeme();
         newTokens.remove(end - 1);
 
@@ -60,7 +63,7 @@ public class NameFinder implements AnalyzerI {
           name = temp + "_" + name;
           newTokens.remove(j);
         }
-        Token token = new TokenImpl(start, end, name);
+        Token token = new TokenImpl(chStart, chEnd, name);
         newTokens.add(start, token);
         
         token.addContext(Analyzers.NAME_FINDER, "P");
