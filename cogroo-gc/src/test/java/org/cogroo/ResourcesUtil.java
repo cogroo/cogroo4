470a2ea1a0725d37ed51450df7007e6a12442a5e
William Colen <william.colen@gmail.com>
2012-12-28 09:03:07 -0200

    Added some unit test

---
+++ cogroo-gc/src/test/java/org/cogroo/ResourcesUtil.java
@@ -0,0 +1,28 @@
+package org.cogroo;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ResourcesUtil {
+	
+	public static File getResourceFile(Class<?> theClass, String fileName) {
+		return new File(theClass.getResource(fileName).getFile());
+	}
+	
+	public static String getResourceAsString(Class<?> theClass, String fileName) throws IOException {
+		
+		File f = new File(theClass.getResource(fileName).getFile());
+		FileReader fr = new FileReader(f);
+		BufferedReader br = new BufferedReader(fr);
+		StringBuilder sb = new StringBuilder();
+		String line = br.readLine();
+		while(line != null) {
+			sb.append(line + "\n");
+			line = br.readLine();
+		}
+		return sb.toString();
+	}
+
+}
