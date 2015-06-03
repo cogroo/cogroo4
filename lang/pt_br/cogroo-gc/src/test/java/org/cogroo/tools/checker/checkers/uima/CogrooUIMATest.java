6a979578397e2bae7a7bd3eb96ee8d79918c4a48
Eduardo S Araujo <duduktamg@hotmail.com>
2015-06-03 15:39:43 -0300

    First version of cogrooUIMA test. #2

---
+++ lang/pt_br/cogroo-gc/src/test/java/org/cogroo/tools/checker/checkers/uima/CogrooUIMATest.java
@@ -0,0 +1,14 @@
+package org.cogroo.tools.checker.checkers.uima;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CogrooUIMATest {
+
+	@Test
+	public void testProcessCAS() {
+		fail("Not yet implemented! AQUI!");
+	}
+
+}
