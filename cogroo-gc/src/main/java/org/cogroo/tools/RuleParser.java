4937f929cb15dd80c105ca93ea973a3334578d14
Vinícius Bitencourt Matos <viniciusbm@users.noreply.github.com>
2015-06-24 15:17:26 -0300

    Local commit: Fixed syntax errors.

--- cogroo-gc/src/main/java/org/cogroo/tools/RuleParser.java
+++ cogroo-gc/src/main/java/org/cogroo/tools/RuleParser.java
@@ -35,18 +35,26 @@ public class RuleParser {
 		try {
 			Map<String, String> rule = new HashMap<String, String>();
 
-			Example example = null;
 			boolean isExample = false;
-			List<Example> examples = new ArrayList<Example>();
+
 			List<String> correctExamples = new ArrayList<String>();
 			List<String> incorrectExamples = new ArrayList<String>();
 			for (String line : Resources.readLines(url, Charsets.UTF_8)) {
 
 				line = line.trim();
 
-				if (line.length() == 0)
-					continue;
+				if (line.length() == 0) {
+					List<Example> examples = new ArrayList<Example>();
+					while (!correctExamples.isEmpty()
+							&& !incorrectExamples.isEmpty()) {
 
+					}
+					rules.add(new JavaRuleDefinition(rule.get("id"), rule
+							.get("category"), rule.get("group"), rule
+							.get("description"), rule.get("message"), rule
+							.get("shortMessage"), examples));
+					continue;
+				}
 				String[] fields = line.split("=", 2);
 
 				if (fields.length < 2)
@@ -54,34 +62,36 @@ public class RuleParser {
 
 				fields[0] = fields[0].trim();
 				fields[1] = fields[1].trim();
-				if (fields[0].equals("correctExample")) {
-					if (!isExample) 
-						example = new Example();
-					isExample = true;
-					example.setCorrect(fields[1]);
-				} else if (fields[0].equals("incorrectExample")) {
-					if (!isExample)
-						example = new Example();
-					isExample = true;
-					example.setIncorrect(fields[1]);
-				}
 
-				if (isExample) {
-					isExample = false;
-					example.setIncorrect(fields[1]);
-					examples.add(example);
-				} else 
-						|| fields[0].equals("incorrectExample")) {
-					
-				} else {
+				if (fields[0].equals("correctExample"))
+					correctExamples.add(fields[1]);
+				else if (fields[0].equals("incorrectExample"))
+					incorrectExamples.add(fields[1]);
+
+				// if (fields[0].equals("correctExample")) {
+				// if (!isExample)
+				// example = new Example();
+				// isExample = true;
+				// example.setCorrect(fields[1]);
+				// } else if (fields[0].equals("incorrectExample")) {
+				// if (!isExample)
+				// example = new Example();
+				// isExample = true;
+				// example.setIncorrect(fields[1]);
+				// }
+				//
+				// if (isExample) {
+				// isExample = false;
+				// example.setIncorrect(fields[1]);
+				// examples.add(example);
+				// } else
+				// || fields[0].equals("incorrectExample")) {
+				//
+				else {
 					rule.put(fields[0], fields[1]);
 				}
 
 			}
-
-			return new JavaRuleDefinition(rule.get("id"), rule.get("category"),
-					rule.get("group"), rule.get("description"),
-					rule.get("message"), rule.get("shortMessage"), examples);
 
 		} catch (IOException e) {
 			e.printStackTrace();
