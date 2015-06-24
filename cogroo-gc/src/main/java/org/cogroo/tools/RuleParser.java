ad149f593d5e2311627661b9e2e47ab847659a40
Vinícius Bitencourt Matos <viniciusbm@users.noreply.github.com>
2015-06-24 15:10:01 -0300

    Merged txt files into one.
    signed-off by: *

--- cogroo-gc/src/main/java/org/cogroo/tools/RuleParser.java
+++ cogroo-gc/src/main/java/org/cogroo/tools/RuleParser.java
@@ -3,8 +3,11 @@ package org.cogroo.tools;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.commons.io.Charsets;
 import org.cogroo.tools.checker.JavaRuleDefinition;
@@ -15,64 +18,75 @@ import com.google.common.io.Resources;
 
 public class RuleParser {
 
-	
 	private RuleParser() {
-		
+
 	}
-	
-	public static RuleDefinition getRuleDefinition(String fileName) {
+
+	public static Set<RuleDefinition> getsRuleDefinition(String fileName) {
+		List<RuleDefinition> rules = new ArrayList<RuleDefinition>();
 		URL url;
-		
+
 		try {
 			url = Resources.getResource(fileName);
+		} catch (Exception e) {
+			return Collections.emptySet();
 		}
-		catch (Exception e) {
-			return null;
-		}
-		
+
 		try {
-			Map<String,String> rule = new HashMap<String,String>();
-			
+			Map<String, String> rule = new HashMap<String, String>();
+
 			Example example = null;
 			boolean isExample = false;
-			ArrayList<Example> examples = new ArrayList<Example>();
-			
+			List<Example> examples = new ArrayList<Example>();
+			List<String> correctExamples = new ArrayList<String>();
+			List<String> incorrectExamples = new ArrayList<String>();
 			for (String line : Resources.readLines(url, Charsets.UTF_8)) {
-				
+
 				line = line.trim();
-				
-				if (line.length() == 0) continue;
-								
+
+				if (line.length() == 0)
+					continue;
+
 				String[] fields = line.split("=", 2);
 
-				if (fields.length < 2) continue;
-							
+				if (fields.length < 2)
+					continue;
+
 				fields[0] = fields[0].trim();
 				fields[1] = fields[1].trim();
-				
+				if (fields[0].equals("correctExample")) {
+					if (!isExample) 
+						example = new Example();
+					isExample = true;
+					example.setCorrect(fields[1]);
+				} else if (fields[0].equals("incorrectExample")) {
+					if (!isExample)
+						example = new Example();
+					isExample = true;
+					example.setIncorrect(fields[1]);
+				}
+
 				if (isExample) {
 					isExample = false;
 					example.setIncorrect(fields[1]);
 					examples.add(example);
-				}
-				else if (fields[0].equals("correctExample")) {
-					isExample = true;
-					example = new Example();
-					example.setCorrect(fields[1]);
-				}
-				else {
+				} else 
+						|| fields[0].equals("incorrectExample")) {
+					
+				} else {
 					rule.put(fields[0], fields[1]);
 				}
-				
+
 			}
-			
-			return new JavaRuleDefinition(rule.get("id"), rule.get("category"), rule.get("group"), rule.get("description"), rule.get("message"), rule.get("shortMessage"), examples);
-		
+
+			return new JavaRuleDefinition(rule.get("id"), rule.get("category"),
+					rule.get("group"), rule.get("description"),
+					rule.get("message"), rule.get("shortMessage"), examples);
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		return null;
 	}
-	
 }
--- cogroo-gc/src/main/java/org/cogroo/tools/checker/checkers/UIMAChecker.java
+++ cogroo-gc/src/main/java/org/cogroo/tools/checker/checkers/UIMAChecker.java
@@ -28,9 +28,7 @@ import org.cogroo.checker.CheckDocument;
 import org.cogroo.checker.GrammarChecker;
 import org.cogroo.entities.Mistake;
 import org.cogroo.entities.Sentence;
-import org.cogroo.tools.RuleParser;
 import org.cogroo.tools.checker.AbstractTypedChecker;
-import org.cogroo.tools.checker.RuleDefinition;
 import org.cogroo.tools.checker.checkers.uima.AnnotatorUtil;
 import org.cogroo.tools.checker.checkers.uima.UimaCasAdapter;
 
@@ -38,88 +36,99 @@ import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
 
 public class UIMAChecker extends AbstractTypedChecker {
-	
+
 	private static final Logger LOGGER = Logger.getLogger(UIMAChecker.class);
-	
+
 	private AnalysisEngine ae;
 	private final UimaCasAdapter converter;
 	private Type mProblemType;
 	private Type mTokenType;
-	private Feature mIDFeature, mSuggestionFeature; 
+	private Feature mIDFeature, mSuggestionFeature;
 
-	
 	private final HashSet<Integer> done = new HashSet<Integer>();
 
 	public UIMAChecker() {
 		// TODO: move the following lines to a new class:
-		// AnalysisEngineFactory: create typesystem and analysis engine 
-		TypeSystemDescription tsd = TypeSystemDescriptionFactory.createTypeSystemDescription("cogroo.ruta.MainTypeSystem");
+		// AnalysisEngineFactory: create typesystem and analysis engine
+		TypeSystemDescription tsd = TypeSystemDescriptionFactory
+				.createTypeSystemDescription("cogroo.ruta.MainTypeSystem");
 		try {
 			URL url = Resources.getResource("cogroo/ruta/Main.ruta");
 			String text = Resources.toString(url, Charsets.UTF_8);
-			AnalysisEngineDescription aeDes = Ruta.createAnalysisEngineDescription(text, tsd);
-			
+			AnalysisEngineDescription aeDes = Ruta
+					.createAnalysisEngineDescription(text, tsd);
+
 			this.ae = UIMAFramework.produceAnalysisEngine(aeDes);
 		} catch (Exception e1) {
 			LOGGER.fatal("Failed to start Ruta AE", e1);
-			throw new RuntimeException("Failed to start Ruta AE",e1);
+			throw new RuntimeException("Failed to start Ruta AE", e1);
 		}
-		
-		int i = 1;
-		while (true) {
-			String filename = i + ".txt";
-			RuleDefinition ruleDef = RuleParser.getRuleDefinition(filename);
 
-			if (ruleDef == null) {
-				break;
-			}
+		// String fileName = "filename.txt";
+		//
+		// URL url = Resources.getResource(fileName);
+		// try {
+		// for (String line : Resources.readLines(url, Charsets.UTF_8)) {
+		// line = line.trim();
+		// RuleDefinition ruleDef = RuleParser.getRuleDefinition(line);
+		//
+		// add(ruleDef);
+		// }
+		// } catch (Exception e) {
+		// e.printStackTrace();
+		// }
 
-			add(ruleDef);
-			i++;
-		}
-		
-		
 		this.converter = new UimaCasAdapter();
 
 	}
 
 	@Override
 	public List<Mistake> check(Sentence sentence) {
-		
-		// TODO: added for now in order to prevent this method to run more than once
+
+		// TODO: added for now in order to prevent this method to run more than
+		// once
 		if (done.contains(sentence.getOffset()))
 			System.exit(0);
 		done.add(sentence.getOffset());
-		
 
 		List<Mistake> mistakes = new LinkedList<Mistake>();
-		
+
 		try {
-			
+
 			CAS cas = ae.newCAS();
-			
+
 			// http://article.gmane.org/gmane.comp.apache.uima.general/6274
 			cas.setDocumentText(sentence.getDocumentText());
-			
-			
+
 			converter.populateCas(sentence.getTextSentence(), cas);
 			ae.process(cas);
-			
+
 			TypeSystem typeSystem = cas.getTypeSystem();
 			initTypeSystem(typeSystem);
 			Set<Integer> processed = new HashSet<Integer>();
-			FSIndex<AnnotationFS> problems = cas.getAnnotationIndex(mProblemType);
+			FSIndex<AnnotationFS> problems = cas
+					.getAnnotationIndex(mProblemType);
 			for (AnnotationFS problem : problems) {
 				if (processed.contains(problem.getBegin()))
 					continue;
 				else
 					processed.add(problem.getBegin());
 				String id = problem.getFeatureValueAsString(mIDFeature);
-				String suggestion = problem.getFeatureValueAsString(mSuggestionFeature);
+				String suggestion = problem
+						.getFeatureValueAsString(mSuggestionFeature);
 				System.out.println("ID = '" + id + "'");
-				System.out.println("TEXTO DO PROBLEM: '" + problem.getCoveredText() + "'");
-				System.out.println("SUGESTÃO: '" +  suggestion + "'");
-//				mistakes.add(createMistake(id, createSuggestion(problem.getCoveredText()), problem.getBegin(), problem.getEnd(), sentence.getSentence()));
+				System.out.println("TEXTO DO PROBLEM: '"
+						+ problem.getCoveredText() + "'");
+				System.out.println("SUGESTÃO: '" + suggestion + "'");
+				// RuleDefinition ruleDef = RuleParser.getRuleDefinition(id
+				// + ".txt");
+				// if (ruleDef != null)
+				// System.out.println("DESCRIÇÃO: '"
+				// + ruleDef.getDescription() + "'");
+				// mistakes.add(createMistake(id,
+				// createSuggestion(problem.getCoveredText()),
+				// problem.getBegin(), problem.getEnd(),
+				// sentence.getSentence()));
 			}
 
 		} catch (Exception e) { // TODO: tratar exceptions corretamente
@@ -127,7 +136,7 @@ public class UIMAChecker extends AbstractTypedChecker {
 		}
 
 		return mistakes;
-	} 
+	}
 
 	private String[] createSuggestion(String error) {
 
@@ -135,23 +144,25 @@ public class UIMAChecker extends AbstractTypedChecker {
 
 		return array;
 	}
-	
+
 	private boolean typeSystemInitialized = false;
-	private synchronized void initTypeSystem(TypeSystem typeSystem) throws AnalysisEngineProcessException {
-		if(typeSystemInitialized == true) {
+
+	private synchronized void initTypeSystem(TypeSystem typeSystem)
+			throws AnalysisEngineProcessException {
+		if (typeSystemInitialized == true) {
 			return;
 		}
 		mProblemType = AnnotatorUtil.getType(typeSystem,
 				"cogroo.ruta.Main.PROBLEM");
 		mIDFeature = AnnotatorUtil.getRequiredFeature(mProblemType, "id",
 				CAS.TYPE_NAME_STRING);
-		mSuggestionFeature = AnnotatorUtil.getRequiredFeature(mProblemType, "suggestion",
-				CAS.TYPE_NAME_STRING);
-//		mTokenType = AnnotatorUtil.getType(typeSystem,
-//				"opennlp.uima.Token");
-//		mLemmaFeature = AnnotatorUtil.getRequiredFeature(mTokenType, "lemma",
-//				CAS.TYPE_NAME_STRING);
-		
+		mSuggestionFeature = AnnotatorUtil.getRequiredFeature(mProblemType,
+				"suggestion", CAS.TYPE_NAME_STRING);
+		// mTokenType = AnnotatorUtil.getType(typeSystem,
+		// "opennlp.uima.Token");
+		// mLemmaFeature = AnnotatorUtil.getRequiredFeature(mTokenType, "lemma",
+		// CAS.TYPE_NAME_STRING);
+
 		typeSystemInitialized = true;
 	}
 
@@ -164,15 +175,19 @@ public class UIMAChecker extends AbstractTypedChecker {
 	public int getPriority() {
 		return 100;
 	}
-	
-	public static void main(String[] args) throws IllegalArgumentException, IOException {
-		ComponentFactory factory = ComponentFactory.create(new Locale("pt", "BR"));
+
+	public static void main(String[] args) throws IllegalArgumentException,
+			IOException {
+		ComponentFactory factory = ComponentFactory.create(new Locale("pt",
+				"BR"));
 		Analyzer cogroo = factory.createPipe();
 		GrammarChecker gc = new GrammarChecker(cogroo);
-//		CheckDocument document = new CheckDocument("Quanto à lápis, não entendo. Quanto à computador, não entendo. Refiro-me à trabalhos remunerados. Refiro-me à reuniões extraordinárias. Fomos levados à crer. A uma hora estaremos partindo. As duas horas estaremos partindo. Os ônibus estacionaram a direita do pátio. Os ônibus estacionaram a esquerda do pátio. Em relação as atividades programadas. Com relação as atividades programadas. Devido as cobranças injustas. Enviei os documentos à você. Enviei os documentos à Vossa Excelência.  Quanto ao lápis, não entendo. Quanto ao computador, não entendo. Refiro-me aos trabalhos remunerados. Refiro-me às reuniões extraordinárias. Refiro-me a reuniões extraordinárias. Fomos levados a crer. À uma hora estaremos partindo. Daqui a uma hora estaremos partindo. Às duas horas estaremos partindo. Os ônibus estacionaram à direita do pátio. Os ônibus estacionaram à esquerda do pátio. Em relação a segurança dos menores. Em relação à segurança dos menores. Em relação às atividades programadas. Com relação a segurança dos menores. Com relação à segurança dos menores. Com relação às atividades programadas. Devido à cobrança injusta. Devido às cobranças injustas. Devido a cobrança injusta. Enviei os documentos a você. Enviei os documentos a Vossa Excelência.");
-		CheckDocument document = new CheckDocument("Já fazem três dias que não o vejo. Fizeram duas semanas que não o vejo.");
+		// CheckDocument document = new
+		// CheckDocument("Quanto à lápis, não entendo. Quanto à computador, não entendo. Refiro-me à trabalhos remunerados. Refiro-me à reuniões extraordinárias. Fomos levados à crer. A uma hora estaremos partindo. As duas horas estaremos partindo. Os ônibus estacionaram a direita do pátio. Os ônibus estacionaram a esquerda do pátio. Em relação as atividades programadas. Com relação as atividades programadas. Devido as cobranças injustas. Enviei os documentos à você. Enviei os documentos à Vossa Excelência.  Quanto ao lápis, não entendo. Quanto ao computador, não entendo. Refiro-me aos trabalhos remunerados. Refiro-me às reuniões extraordinárias. Refiro-me a reuniões extraordinárias. Fomos levados a crer. À uma hora estaremos partindo. Daqui a uma hora estaremos partindo. Às duas horas estaremos partindo. Os ônibus estacionaram à direita do pátio. Os ônibus estacionaram à esquerda do pátio. Em relação a segurança dos menores. Em relação à segurança dos menores. Em relação às atividades programadas. Com relação a segurança dos menores. Com relação à segurança dos menores. Com relação às atividades programadas. Devido à cobrança injusta. Devido às cobranças injustas. Devido a cobrança injusta. Enviei os documentos a você. Enviei os documentos a Vossa Excelência.");
+		CheckDocument document = new CheckDocument(
+				"Arquivos branco. O Eclipse é boa ferramenta.");
 		// passe o doc pelo pipe
 		gc.analyze(document);
-	
+
 	}
 }
