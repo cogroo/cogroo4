0d19f1ade617a3409231d5e17281a510df4fc17f
William Colen <william.colen@gmail.com>
2016-11-23 14:09:29 -0200

    Add unit test for FSADictionary

--- lang/pt_br/cogroo-ann/src/test/java/org/cogroo/dictionary/impl/FSADictionaryTest.java
+++ lang/pt_br/cogroo-ann/src/test/java/org/cogroo/dictionary/impl/FSADictionaryTest.java
@@ -10,13 +10,14 @@ import org.junit.Test;
 public class FSADictionaryTest {
 
   @Test
-  public void testSimpleCount() throws IOException {
-    String path = "/fsa_dictionaries/pos/pt_br_jspell_corpus";
+  public void testLemmatizerDict() throws IOException {
+    String path = "/fsa_dictionaries/pos/pt_br_jspell";
     InputStream dict = FSADictionary.class.getResourceAsStream(path + ".dict");
     InputStream info = FSADictionary.class.getResourceAsStream(path + ".info");
     
     FSADictionary td = FSADictionary.create(dict, info);
     
-    System.out.println(td.getTagsAndLemms("d"));;
+    assertEquals("menino", td.getLemmas("meninos", "n")[0]);
+    
   }
 }
